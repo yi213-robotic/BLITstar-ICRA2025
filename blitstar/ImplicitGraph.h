3053fa70014d3943b3c0dc74d4abb6bd0a12333c
Yi Wang <54214597+yi213-robotic@users.noreply.github.com>
2025-05-28 09:16:34 -0400

    Update ImplicitGraph.h

--- blitstar/ImplicitGraph.h
+++ blitstar/ImplicitGraph.h
@@ -1,7 +1,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2019, University of New Hampshire
+ *  Copyright (c) 2025, University of New Hampshire
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -254,4 +254,4 @@ namespace ompl
 
 }  // namespace ompl
 
-#endif  // OMPL_GEOMETRIC_PLANNERS_INFORMEDTREES_bmitstar_IMPLICITGRAPH_
+#endif  // OMPL_GEOMETRIC_PLANNERS_INFORMEDTREES_blitstar_IMPLICITGRAPH_
